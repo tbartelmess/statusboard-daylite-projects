06cb52729509edc68952a0fbe99aefd198cd1ab4
Petr Havelka <petr.havelka@web-entity.cz>
2013-04-21 15:09:46 +0200

    Added option for count of work days instead of due date

--- dayliteProjects.php
+++ dayliteProjects.php
@@ -31,42 +31,42 @@ $columns = array(
 $widthUnit = 'px';
 
 // category filter - what categories to show
-define('FILTER_BY_CATEGORY', false);  //set to true if you whant to filter by categories
+define('FILTER_BY_CATEGORY', false);  //set it to true if you want to filter by categories
 $categories = array(
   'category1',
   'category2'
 );
 
 // owner filter - show project with these owners
-define('FILTER_BY_OWNER', false); //set to true if you whant to filter by owner
+define('FILTER_BY_OWNER', false); //set it to true if you want to filter by owner
 $owners = array(
   'owner1',
   'owner2'
 );
 
 // client filter - show project with these clients
-define('FILTER_BY_CLIENT', false); //set to true if you whant to filter by client
+define('FILTER_BY_CLIENT', false); //set it to true if you want to filter by client
 $clients = array(
   'client1',
   'client2'
 );
 
 // exclude category - what categories to not show
-define('EXCLUDE_CATEGORY', false);  //set to true if you whant to use it
+define('EXCLUDE_CATEGORY', false);  //set it to true if you want to use it
 $ex_categories = array(
   'category1',
   'category2'
 );
 
 // exclude owner - not show project with these owners
-define('EXCLUDE_OWNER', false); //set to true if you whant to use it
+define('EXCLUDE_OWNER', false); //set it to true if you want to use it
 $ex_owners = array(
   'owner1',
   'owner2'
 );
 
 // exclude client - not show project with these clients
-define('EXCLUDE_CLIENT', false); //set to true if you whant to use it
+define('EXCLUDE_CLIENT', false); //set it to true if you want to use it
 $ex_clients = array(
   'client1',
   'client2'
@@ -74,7 +74,7 @@ $ex_clients = array(
 
 
 // order by priority - default is by due date
-define('ORDER_BY_PRIORITY', false); //set to true if you whant to use it
+define('ORDER_BY_PRIORITY', false); //set it to true if you want to use it
 
 // min days to show as red
 $redDays = 7;
@@ -84,29 +84,29 @@ $redColor = "red"; //css color
 define('LOAD_AWESOME', false);
 
 //owners nicknames
-define('USE_OWNER_NICKNAMES', false); //set to true if you whant to use it
-define('USE_OWNER_IMG', false); //set to true, if you like to use images instead of text. USE_OWNER_NICKNAMES must be true. Add only path to image with image name (for example /images/mary.jpg).
+define('USE_OWNER_NICKNAMES', false); //set it to true if you want to use it
+define('USE_OWNER_IMG', false); //set it to true, if you like to use images instead of text. USE_OWNER_NICKNAMES must be true. Add only path to image with image name (for example /images/mary.jpg).
 $nicknames = array(
   'Mary Jennings' => 'Mary'
 );
 
 //client nicknames
-define('USE_CLIENT_NICKNAMES', false); //set to true if you whant to use it
-define('USE_CLIENT_IMG', false); //set to true, if you like to use images instead of text. USE_CLIENT_NICKNAMES must be true. Add only path to image with image name (for example /images/mary.jpg).
+define('USE_CLIENT_NICKNAMES', false); //set it to true if you want to use it
+define('USE_CLIENT_IMG', false); //set it to true, if you like to use images instead of text. USE_CLIENT_NICKNAMES must be true. Add only path to image with image name (for example /images/mary.jpg).
 $clientnicknames = array(
   'Chuck Darwin' => 'Chuck'
 );
 
 //category nicknames
-define('USE_CAT_NICKNAMES', false); //set to true if you whant to use it
-define('USE_CAT_IMG', false); //set to true, if you like to use images instead of text. USE_CAT_NICKNAMES must be true. Add only path to image with image name (for example /images/mary.jpg).
+define('USE_CAT_NICKNAMES', false); //set it to true if you want to use it
+define('USE_CAT_IMG', false); //set it to true, if you like to use images instead of text. USE_CAT_NICKNAMES must be true. Add only path to image with image name (for example /images/mary.jpg).
 $catnicknames = array(
   'Marketing' => '[M]'
 );
 
 //company nicknames
-define('USE_COM_NICKNAMES', false); //set to true if you whant to use it
-define('USE_COM_IMG', false); //set to true, if you like to use images instead of text. USE_COM_NICKNAMES must be true. Add only path to image with image name (for example /images/mary.jpg).
+define('USE_COM_NICKNAMES', false); //set it to true if you want to use it
+define('USE_COM_IMG', false); //set it to true, if you like to use images instead of text. USE_COM_NICKNAMES must be true. Add only path to image with image name (for example /images/mary.jpg).
 $comnicknames = array(
   'Darwin Studios' => '(D)'
 );
@@ -116,13 +116,17 @@ define('SHOW_HEADLINE', false);
 $headColor = ''; //headline text color
 
 // show percent done in bars - you need to show percentDoneString in columns
-define('SHOW_BARS', true); //set to true if you whant to use it
+define('SHOW_BARS', true); //set it to true if you want to use it
 
 //Date format for dueDate - must be same as Daylites ouput - see http://php.net/manual/en/function.date.php
 $dateFormat = "j. n. Y";
 
 // shot for example 1 month, 2 days instead of 10.5.2013. You must have correct $dateFormat .
-define('SHOW_TO_DATE', false); //set to true if you whant to use it
+define('SHOW_TO_DATE', false); //set it to true if you want to use it
+
+define('SHOW_WORK_DAYS', false); //set it to true if you want to show count of work days
+$holidaysDateFormat = 'Y-m-d'; //date format for holidays - more at http://www.php.net/manual/en/datetime.formats.date.php
+$holidays = array('2013-05-01');
 
 /************************************************
  */
@@ -317,15 +321,17 @@ function table(){
 
           }
 
-          //setup color
+          //color
           if(!empty($color)) {
             $color = 'color: '.$color.';';
           }
 
+          //width
           if(!empty($width)) {
             $width = 'width: '.$width.$widthUnit.';';
           }
 
+          //align
           if(!empty($align)) {
             $align = 'text-align: '.$align.';';
           }
@@ -352,6 +358,7 @@ function table(){
 function get_date_content($date) {
   if(!empty($date)) {
     global $dateFormat;
+    global $holidaysDateFormat;
     global $redDays;
     global $redColor;
 
@@ -385,11 +392,15 @@ function get_date_content($date) {
       $to .= ' ago';
     }
 
-    if(!SHOW_TO_DATE) {
-      $to = $date;
+    if(SHOW_TO_DATE) {
+      $output = $to;
+    } else if(SHOW_WORK_DAYS) {
+      $output = getWorkingDays($curr->format($holidaysDateFormat), $due->format($holidaysDateFormat));
+    } else {
+      $output = $date;
     }
 
-    return array($to,$color);
+    return array($output,$color);
 
   } else {
     return false;
@@ -408,6 +419,72 @@ function priority_sort($a, $b) {
 function calc_bars($percent){
 	$value = (int) str_replace('%', '', $percent) / 10;
 	return round( ($value > 8 ? 8 : $value) * .8 );
+}
+
+//Source http://stackoverflow.com/questions/336127/calculate-business-days
+//The function returns the no. of business days between two dates and it skips the holidays
+function getWorkingDays($startDate,$endDate){
+    global $holidays;
+    // do strtotime calculations just once
+    $endDate = strtotime($endDate);
+    $startDate = strtotime($startDate);
+
+
+    //The total number of days between the two dates. We compute the no. of seconds and divide it to 60*60*24
+    //We add one to inlude both dates in the interval.
+    $days = ($endDate - $startDate) / 86400 + 1;
+
+    $no_full_weeks = floor($days / 7);
+    $no_remaining_days = fmod($days, 7);
+
+    //It will return 1 if it's Monday,.. ,7 for Sunday
+    $the_first_day_of_week = date("N", $startDate);
+    $the_last_day_of_week = date("N", $endDate);
+
+    //---->The two can be equal in leap years when february has 29 days, the equal sign is added here
+    //In the first case the whole interval is within a week, in the second case the interval falls in two weeks.
+    if ($the_first_day_of_week <= $the_last_day_of_week) {
+        if ($the_first_day_of_week <= 6 && 6 <= $the_last_day_of_week) $no_remaining_days--;
+        if ($the_first_day_of_week <= 7 && 7 <= $the_last_day_of_week) $no_remaining_days--;
+    }
+    else {
+        // (edit by Tokes to fix an edge case where the start day was a Sunday
+        // and the end day was NOT a Saturday)
+
+        // the day of the week for start is later than the day of the week for end
+        if ($the_first_day_of_week == 7) {
+            // if the start date is a Sunday, then we definitely subtract 1 day
+            $no_remaining_days--;
+
+            if ($the_last_day_of_week == 6) {
+                // if the end date is a Saturday, then we subtract another day
+                $no_remaining_days--;
+            }
+        }
+        else {
+            // the start date was a Saturday (or earlier), and the end date was (Mon..Fri)
+            // so we skip an entire weekend and subtract 2 days
+            $no_remaining_days -= 2;
+        }
+    }
+
+    //The no. of business days is: (number of weeks between the two dates) * (5 working days) + the remainder
+//---->february in none leap years gave a remainder of 0 but still calculated weekends between first and last day, this is one way to fix it
+   $workingDays = $no_full_weeks * 5;
+    if ($no_remaining_days > 0 )
+    {
+      $workingDays += $no_remaining_days;
+    }
+
+    //We subtract the holidays
+    foreach($holidays as $holiday){
+        $time_stamp=strtotime($holiday);
+        //If the holiday doesn't fall in weekend
+        if ($startDate <= $time_stamp && $time_stamp <= $endDate && date("N",$time_stamp) != 6 && date("N",$time_stamp) != 7)
+            $workingDays--;
+    }
+
+    return $workingDays;
 }
 
 ?>
